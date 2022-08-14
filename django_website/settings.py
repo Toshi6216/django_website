7eec38286f6190c0311bc946e0d5d3b97078d46d
Your Name <you@example.com>
2022-08-14 16:32:55 +0900

    update

--- django_website/settings.py
+++ django_website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v6u=fgl3a8lmeibes%hj0d7+g%a-)nqqls^h-difny66kpbtug'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
